5f659fb13ed1498562e6f29e6f8c8d76db5e50f4
Christian Parrish <69486926+A01751584@users.noreply.github.com>
2022-03-09 22:46:27 +0000

    Webpack Construction  09/03/2022

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import add from './add';
 // LAB: Importar subtraction
+import subtraction from './subtraction';
 
 
 let result = add(4, 5);
@@ -9,5 +10,9 @@ if (output) {
 }
 
 // LAB: Invocar subtraction
-
+result = subtraction(10, 2);
+output = document.querySelector('#sub_result');
+if (output) {
+    output.innerHTML = result.toString();
+}
 
